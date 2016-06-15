15581b10757164af5c7f900b487ab85c5ec431cb
deepb <github.9.deepb@spamgourmet.com>
2016-06-15 06:07:03 +0200

    modified:   crud.py 	modified:   ui/crud.glade

--- crud.py
+++ crud.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-file: crud.py
-version: 0.0001
+file: crud.py $Id$
+version: 0.0001 $Date$
 
     LICENCIA PÚBLICA PARA QUE HAGA LO QUE LE DÉ LA GANA
                     Versión 2, diciembre de 2004
@@ -39,38 +39,10 @@ class Aplicacion(Gtk.Window):
         b = Gtk.Builder()
         b.add_from_file('ui/crud.glade')
         
-        self.status = b.get_object("staStatus")
-        self._id = self.status.get_context_id("CRUD")
-
-        self._handlers = {
-            "onDeleteWindow": self.deleteWindow,
-            "on_btnCreate_clicked": self.createBBDD, 
-            "on_btnRemove_clicked": self.removeBBDD, 
-            "on_btnUpdate_clicked": self.updateRecord, 
-            "on_btnDelete_clicked": self.deleteRecord, 
-            "on_btnAbout_clicked": self.onOpenAbout, 
-            "on_btnExit_clicked": self.deleteWindow, 
-            "on_mnuCreate_activate": self.createBBDD, 
-            "on_mnuRemove_activate": self.removeBBDD, 
-            "on_mnuUpdate_activate": self.updateRecord, 
-            "on_mnuDelete_activate": self.deleteRecord, 
-            "on_mnuAbout_activate": self.onOpenAbout, 
-            "on_mnuQuit_activate": self.deleteWindow,
-            "on_dlgAcerca_response": self.onCloseAbout,
-            "on_tsSelect_changed": self.changedRecord, 
-            "on_treNombre_edited": self.updateRecord, 
-            "on_treApellidos_edited": self.updateRecord, 
-            "on_treDNI_edited": self.updateRecord, 
-            "on_treVivienda_edited": self.updateRecord, 
-            
-            "test": self.updateStatus(__doc__)
-        }
-        b.connect_signals(self._handlers)
-        
         # Ventanas
         self.ventana = b.get_object("winBBDD")
         self.acerca = b.get_object("dlgAcerca")
-        self.acerca.set_transient_for(self.ventana)
+        self.registro = b.get_object("winRegistro")
         
         # Objetos
         self.btnCreate = b.get_object("btnCreate")
@@ -82,10 +54,40 @@ class Aplicacion(Gtk.Window):
         self.mnuRemove = b.get_object("mnuRemove")
         self.mnuUpdate = b.get_object("mnuUpdate")
         self.mnuDelete = b.get_object("mnuDelete")
+
+        self.status = b.get_object("staStatus")
+        self._id = self.status.get_context_id("CRUD")
         
         # Datos
         self.model = b.get_object("lsCrud")
         self.list = b.get_object("treBBDD")
+
+        # Handlers
+        self._handlers = {
+            "onDeleteWindow": self.deleteWindow,
+            "on_btnCreate_clicked": self.createBBDD, 
+            "on_btnRemove_clicked": self.removeBBDD, 
+            "on_btnUpdate_clicked": self.onOpenRegistro, 
+            "on_btnDelete_clicked": self.deleteRecord, 
+            "on_btnAbout_clicked": self.onOpenAbout, 
+            "on_btnExit_clicked": self.deleteWindow, 
+            "on_mnuCreate_activate": self.createBBDD, 
+            "on_mnuRemove_activate": self.removeBBDD, 
+            "on_mnuUpdate_activate": self.onOpenRegistro, 
+            "on_mnuDelete_activate": self.deleteRecord, 
+            "on_mnuAbout_activate": self.onOpenAbout, 
+            "on_mnuQuit_activate": self.deleteWindow,
+            "on_dlgAcerca_response": self.onCloseAbout,
+            "on_tsSelect_changed": self.changedRecord, 
+            "on_treNombre_edited": self.updateRecord, 
+            "on_treApellidos_edited": self.updateRecord, 
+            "on_treDNI_edited": self.updateRecord, 
+            "on_treVivienda_edited": self.updateRecord, 
+            "on_btnCancel_clicked": self.onCloseRegistro, 
+            "on_btnAdd_clicked": self.updateRecord
+        }
+        b.connect_signals(self._handlers)
+        
         
         # Init BBDD
         self.initBBDD()
@@ -99,11 +101,17 @@ class Aplicacion(Gtk.Window):
 
     # Bases de datos
     def initBBDD(self, *args):
+        self.disable(self.btnRemove)
+        self.disable(self.mnuRemove)
+        self.disable(self.btnUpdate)
+        self.disable(self.btnDelete)
+        self.disable(self.mnuUpdate)
+        self.disable(self.mnuDelete)
         try:
             self.db = db.connect(db_host, db_user, db_pass, db_data)
         except:
             self.db = None
-            self.updateStatus("ERROR EN MYSQL")
+            self.updateStatus("Error MySQL: Revisa los datos de conexión")
 
     def createBBDD(self, *args):
         query = "CREATE TABLE IF NOT EXISTS "\
@@ -137,12 +145,19 @@ class Aplicacion(Gtk.Window):
             self.updateStatus("Error: BBDD no eliminada")
 
     def updateRecord(self, *args):
+        self.onCloseRegistro()
         self.updateStatus("Registro actualizado")
 
     def deleteRecord(self, *args):
+        self.disable(self.btnDelete)
+        self.disable(self.mnuDelete)
         self.updateStatus("Registro eliminado")
 
     def changedRecord(self, *args):
+        self.enable(self.btnUpdate)
+        self.enable(self.mnuUpdate)
+        self.enable(self.btnDelete)
+        self.enable(self.mnuDelete)
         self.updateStatus("Cambiado registro")
 
     # Ventanas
@@ -155,6 +170,10 @@ class Aplicacion(Gtk.Window):
         self.acerca.show()
     def onCloseAbout(self, *args):
         self.acerca.hide()
+    def onOpenRegistro(self, *args):
+        self.registro.show()
+    def onCloseRegistro(self, *args):
+        self.registro.hide()
 
     # Ventana principal
     def deleteWindow(self, *args):
